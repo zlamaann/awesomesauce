b915bfb326ea75d5072d63d94f53439526082788
zlamaann <andapanda.az@gmail.com>
2022-09-20 01:36:33 +0200

    delete frontend from the main folder

--- src/components/About.tsx
+++
@@ -1,7 +0,0 @@
-import { Component, FC } from "react";
-
-const About: FC = () => {
-    return (
-      <div className="main about">Awesome app for creating (p)awsome articles!</div>
-    );
-};
--- src/redux/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './store/store';
-export * as articleCreators from './reducers/articleReducer'
